82d213f5209fa57006a1482b9767bb5d0e36587a
Shannon Mackey <noreturn86@gmail.com>
2025-12-09 14:37:10 -0500

    Adding patient register amd login function

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PatientLayout from "./pages/PatientLayout";
 import PatientMain from "./pages/PatientMain";
 import PatientBook from "./pages/PatientBook";
+import PatientRegister from "./pages/PatientRegister";
 
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<PatientRegister />} />
         <Route element={<PatientLayout />}>
-          <Route path="/" element={<PatientMain />} />
+          <Route path="/main" element={<PatientMain />} />
           <Route path="/book" element={<PatientBook />} />
         </Route>
       </Routes>
--- src/pages/PatientBook.jsx
+++ src/pages/PatientBook.jsx
@@ -1,3 +1,21 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function PatientBook() {
-  return <h2 className="text-xl">Book Appointment Page</h2>;
+    const [availableSLots, setAvailableSlots] = useState(null);
+
+    useEffect(() => {
+        async function getAvailableSlots() {
+            try {
+
+            } catch (err){
+                console.error("Error fetching slots:", err);
+            }
+        }
+
+        getAvailableSlots();
+    }, []);
+
+
+    return <h2 className="text-xl">Book Appointment Page</h2>;
 }
